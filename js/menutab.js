607ea41873d820e9a7c41ef14132250d7fb0b95f
yun9612 <1226choiy@gmail.com>
2025-08-22 13:03:12 +0900

    탭해서 배너 이미지 바뀌기, 헤더 열고 닫는 버튼 넣기

---
+++ js/menutab.js
@@ -0,0 +1,37 @@
+window.addEventListener("load", function () {
+  const popupBtn = this.document.querySelector(".popUpBtn");
+  const popupText = this.document.querySelector(".popupText");
+  const popupLink = this.document.querySelector(".popUpBtn .inner a");
+  popupBtn.addEventListener("click", function () {
+    // 삼항연산자, if문으로 써도 됨
+    // popupText.style.display = popupText.style.display === "none" ? "block" : "none";
+    if (popupText.style.display === "none") {
+      popupText.style.display = "block";
+      popupLink.classList.remove("active");
+    } else {
+      popupText.style.display = "none";
+      popupLink.classList.add("active");
+    }
+  });
+  // 탭기능
+  const tabTitles = this.document.querySelectorAll(".tabNav > li > a");
+  const tabItems = this.document.querySelectorAll(".tabItem");
+  // 첫번째 탭 활성화
+  tabTitles[0].classList.add("active");
+  tabItems[0].style.display = "block";
+  // 탭 기능 코드
+  tabTitles.forEach((tabTitle, index) => {
+    tabTitle.addEventListener("click", () => {
+      // 모든 탭과 내용을 초기화하는 코드
+      tabTitles.forEach((title) => {
+        title.classList.remove("active");
+      });
+      tabItems.forEach((item) => {
+        item.style.display = "none";
+      });
+      // 모든 탭과 내용을 보이게하는 코드
+      tabTitle.classList.add("active");
+      tabItems[index].style.display = "block";
+    });
+  });
+}); //END
